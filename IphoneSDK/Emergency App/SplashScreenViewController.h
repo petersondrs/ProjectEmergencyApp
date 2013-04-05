8ed3b57ff837d3131fa6e9ce155dfcb107950c79
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2013-04-05 20:14:08 -0300

    Criação da Pagina de Splash Screen

---
+++ IphoneSDK/Emergency App/SplashScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  SplashScreenViewController.h
+//  Emergency App
+//
+//  Created by Rafael Bento Cruz on 05/04/13.
+//  Copyright (c) 2013 Rafael Bento Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashScreenViewController : UIViewController
+
+@end
