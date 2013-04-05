b586e5811031d48c3a631e9a575ca776a3a9eb2a
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2013-04-05 19:24:45 -0300

    Commit dos arquivos do projeto

---
+++ IphoneSDK/Emergency App/Emergency App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Emergency App
+//
+//  Created by Rafael Bento Cruz on 05/04/13.
+//  Copyright (c) 2013 Rafael Bento Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IphoneSDK/Emergency App/Emergency App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Emergency App
+//
+//  Created by Rafael Bento Cruz on 05/04/13.
+//  Copyright (c) 2013 Rafael Bento Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
