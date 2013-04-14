8a30fd1268e960b841451981ef07f878bcc8776a
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2013-04-14 18:19:18 -0300

    Adição das Views de Configurações

---
+++ IphoneSDK/Emergency App/Emergency App/ServiceTermViewController.h
@@ -0,0 +1,13 @@
+//
+//  ServiceTermViewController.h
+//  Emergency App
+//
+//  Created by Rafael Bento Cruz on 14/04/13.
+//  Copyright (c) 2013 Rafael Bento Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServiceTermViewController : UIViewController
+
+@end
---
+++ IphoneSDK/Emergency App/Emergency App/ServiceTermViewController.m
@@ -0,0 +1,38 @@
+//
+//  ServiceTermViewController.m
+//  Emergency App
+//
+//  Created by Rafael Bento Cruz on 14/04/13.
+//  Copyright (c) 2013 Rafael Bento Cruz. All rights reserved.
+//
+
+#import "ServiceTermViewController.h"
+
+@interface ServiceTermViewController ()
+
+@end
+
+@implementation ServiceTermViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
