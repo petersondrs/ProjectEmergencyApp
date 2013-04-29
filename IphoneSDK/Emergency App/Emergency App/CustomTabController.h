872c3333f35c8a39d1a1b36cb4deca851e865ff8
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2013-04-28 22:02:40 -0300

    Criação do Custom Tab Bar

---
+++ IphoneSDK/Emergency App/Emergency App/CustomTabController.h
@@ -0,0 +1,13 @@
+//
+//  CustomTabController.h
+//  Emergency App
+//
+//  Created by Rafael Bento Cruz on 4/28/13.
+//  Copyright (c) 2013 Rafael Bento Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTabController : UITabBarController
+
+@end
---
+++ IphoneSDK/Emergency App/Emergency App/CustomTabController.m
@@ -0,0 +1,90 @@
+//
+//  CustomTabController.m
+//  Emergency App
+//
+//  Created by Rafael Bento Cruz on 4/28/13.
+//  Copyright (c) 2013 Rafael Bento Cruz. All rights reserved.
+//
+
+#import "CustomTabController.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface CustomTabController ()
+
+@end
+
+@implementation CustomTabController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    [self topTabBarItem];
+    
+}
+
+- (UIButton *)createCustomButton:(UIImage *)buttonImage
+{
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    button.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
+    
+    button.frame = CGRectMake(20, -17, buttonImage.size.width, buttonImage.size.height);
+    
+    [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
+   
+    return button;
+}
+
+-(void) topTabBarItem
+{
+    
+    UIImage* buttonImage = [UIImage imageNamed:@"camera_button_take.png"];
+    NSArray* tabViews = [self.tabBar subviews];
+    
+    int index = 0;
+    
+    for (UIView* view in tabViews)
+    {
+        if ([view isKindOfClass:NSClassFromString(@"UITabBarButton")])
+        {
+            UIButton *button;
+            button = [self createCustomButton:buttonImage];
+            button.tag = index;
+            [button addTarget:self action:@selector(btnCustomButton_TouchUpInside:) forControlEvents:UIControlEventTouchUpInside];
+            [button sizeToFit];
+            [view addSubview:button];
+            index++;
+        }
+        
+    }
+    
+}
+
+-(void) btnCustomButton_TouchUpInside:(id)sender
+{
+    UIButton* btn = (UIButton*)sender;
+    self.selectedIndex = btn.tag;
+}
+
+
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
